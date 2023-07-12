85e00537af2a5b30cd2e7b897cb1d68f55064b36
Nir Neuman <nir.neuman@icloud.com>
2023-07-12 14:30:43 +0300

    Add Firebase to project

--- FindCar/FindCarApp.swift
+++ FindCar/FindCarApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// Added class for Firebase setup
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct FindCarApp: App {
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
