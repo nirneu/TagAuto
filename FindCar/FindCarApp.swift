e2096f1c10193213ecdc0279e0d40aef3b597c01
Nir Neuman <nir.neuman@icloud.com>
2023-07-12 13:42:05 +0300

    Initial Commit

---
+++ FindCar/FindCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCarApp.swift
+//  FindCar
+//
+//  Created by Nir Neuman on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
