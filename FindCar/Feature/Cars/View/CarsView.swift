852cd4340b4d2d0346069fa2c1d5e030b902503c
Nir Neuman <nir.neuman@icloud.com>
2023-08-10 19:55:12 +0300

    Cars rows now clickable and shows a background but when clicking on current location the background won't go off.

--- FindCar/Feature/Cars/View/CarsView.swift
+++ FindCar/Feature/Cars/View/CarsView.swift
@@ -13,8 +13,11 @@ struct CarsView: View {
     @EnvironmentObject var carsViewModel: CarsViewModelImpl
     
     @State private var showLocationUpdateAlert = false
+    @State private var carToUpdate: Car?
+    @State private var selectedCar: Car?
     
     var body: some View {
+        
         VStack(alignment: .leading, spacing: 0) {
             
             Text("Cars")
@@ -30,61 +33,65 @@ struct CarsView: View {
                     if !carsViewModel.cars.isEmpty {
                         
                         ForEach(carsViewModel.cars, id: \.self) { car in
+                            
                             HStack {
-                                Image(systemName: "car.fill")
-                                Text(car.name)
                                 
-                                Spacer()
-                                
-                                HStack(spacing: 20) {
-                                    
-                                    Button {
-                                        self.showLocationUpdateAlert = false
-                                        carsViewModel.selectCar(car)
-                                    } label: {
-                                        Image(systemName: "location.fill")
-                                    }
-                                    .buttonStyle(.borderless)
-                                    
-                                    
-                                    Divider()
-                                    Button {
-                                        carsViewModel.selectCar(car)
-                                        self.showLocationUpdateAlert = true
-                                    } label: {
-                                        Image(systemName: "mappin.and.ellipse")
-                                    }
-                                    .buttonStyle(.borderless)
-
-
-                                    .alert("Location Update", isPresented: $showLocationUpdateAlert) {
-                                        Button("Confirm", action: {
-                                            carsViewModel.updateCarLocation(car)
-                                            if let userId = sessionService.userDetails?.userId {
-                                                carsViewModel.fetchUserCars(userId: userId)
-                                            }
-                                            self.showLocationUpdateAlert = false
-                                        })
-                                        Button("Cancel", role: .cancel) {
-                                            self.showLocationUpdateAlert = false
-                                        }
-                                    } message: {
-                                        Text("Set the car's location to your current position?")
+                                Button {
+                                    self.showLocationUpdateAlert = false
+                                    carsViewModel.selectCar(car)
+                                    selectedCar = car
+                                } label: {
+                                    HStack {
+                                        Image(systemName: "car.fill")
+                                        Text(car.name)
                                     }
                                 }
+                                                    
+                                Spacer()
+                                          
+                                Divider()
+                                
+                                Button {
+                                    carToUpdate = car
+                                    self.showLocationUpdateAlert = true
+                                } label: {
+                                    Image(systemName: "mappin.and.ellipse")
+                                }
+                                .buttonStyle(.borderless)
+                                
                             }
+                            .listRowBackground(selectedCar == car ? Color.gray.opacity(0.3) : Color.clear)
                         }
                         .frame(height: 40)
+
+                        
                     } else {
                         
                         Text("You still don't have cars. Go ahead for the Groups section to add one.")
                     }
                 }
             }
+            .alert("Location Update", isPresented: $showLocationUpdateAlert) {
+                Button("Confirm", action: {
+                    if let updatingCar = carToUpdate {
+                        carsViewModel.updateCarLocation(updatingCar)
+                        if let userId = sessionService.userDetails?.userId {
+                            carsViewModel.fetchUserCars(userId: userId)
+                        }
+                        self.showLocationUpdateAlert = false
+                    }
+                })
+                Button("Cancel", role: .cancel) {
+                    self.showLocationUpdateAlert = false
+                }
+            } message: {
+                Text("Set the car's location to your current position?")
+            }
             .onAppear {
                 if let userId = sessionService.userDetails?.userId {
                     carsViewModel.fetchUserCars(userId: userId)
                 }
+                selectedCar = nil
             }
             .onChange(of: sessionService.userDetails) { newUserDetails in
                 if let userId = newUserDetails?.userId {
@@ -92,7 +99,6 @@ struct CarsView: View {
                 }
             }
             .listStyle(.plain)
-            
             .alert("Error", isPresented: $carsViewModel.hasError) {
                 Button("OK", role: .cancel) { }
             } message: {
@@ -117,6 +123,6 @@ struct CarsView_Previews: PreviewProvider {
         CarsView()
             .environmentObject(sessionService)
             .environmentObject(carsViewModel)
-
+        
     }
 }
