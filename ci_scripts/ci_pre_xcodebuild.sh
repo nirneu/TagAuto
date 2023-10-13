c092d48a644f478fdeeb423a47ce0f1e87c6b0ed
Nir Neuman <nir.neuman@icloud.com>
2023-10-13 16:23:45 +0200

    Trying to solve server_key not accessible

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+#  ci_pre_xcodebuild.sh
+#  FinnFinds
+#
+#  Created by Nir Neuman on 13/10/2023.
+#
+
 echo "Stage: PRE-Xcode Build is activated .... "
 
-cd $CI_WORKSPACE/ci_scripts || exit 1
+cd ../FinnFinds/
 
 plutil -replace SERVER_KEY -string $SERVER_KEY Info.plist
 
